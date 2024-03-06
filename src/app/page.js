a2403c8cacc8df9967f440b600c34ffa9696a4ff
StackHustla <153138763+StackHustla@users.noreply.github.com>
2024-03-06 17:28:24 -0600

    REACT is phenomenal
    As a professional designer I can see why people use this along with tailwind. It's ridiculously powerful

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,21 @@
 import Image from "next/image";
+import {BsFillMoonStarsFill} from 'react-icons/bs'
+import { BiAccessibility } from "react-icons/bi";
 
 export default function Home() {
   return (
-    <main >
+    <main className="bg-yellow-700 px-10 ">
       <div >
-        
+        <section className=" bg-yellow-500 min-h-screen">
+          <nav className="py-10 mb-12 flex justify-between">
+            <h1 className="text-xl">devlopedbyed</h1>
+            <ul className="flex items-center">
+              <li className="cursor-pointer text-2xl "><BsFillMoonStarsFill/></li>
+              <li className=" bg-cyan-500 text-white px-4 py-2 rounded-md ml-8"><a href="#">Resume</a></li>
+            </ul>
+          </nav>
+
+        </section>
       </div>
     </main>
   );
