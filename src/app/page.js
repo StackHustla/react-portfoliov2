2345aa7c229bd0e8f8af75e7c1de82d024aaf6ac
StackHustla <153138763+StackHustla@users.noreply.github.com>
2024-03-11 17:37:15 -0500

    Almost done with the basis.
    Going to add my own art, themes and spin asap!

--- src/app/page.js
+++ src/app/page.js
@@ -2,12 +2,23 @@ import { BsFillMoonStarsFill } from 'react-icons/bs'
 import { BiAccessibility } from "react-icons/bi";
 import Carousel from "./components/carousel";
 import { AiFillTwitterCircle, AiFillLinkedin, AiFillYoutube } from 'react-icons/ai';
-import Image  from "next/image";
+import Image from "next/image";
 import sukuna from '/public/sukuna.jpeg';
+import design from '/public/design.png';
+import code from '/public/code.png';
+import consulting from '/public/consulting.png';
+import web1 from '/public/web1.png';
+import web2 from '/public/web2.png';
+import web3 from '/public/web3.png';
+import web4 from '/public/web4.png';
+import web5 from '/public/web5.png';
+import web6 from '/public/web6.png';
+
+
 
 export default function Home() {
   return (
-    <main>
+    <main className='bg-white px-10  md:px-20 lg:px-40'>
       <div>
         <section className=" bg-white min-h-screen">
           <nav className="py-10 mb-12 flex justify-between">
@@ -18,9 +29,9 @@ export default function Home() {
             </ul>
           </nav>
           <div className="text-center p-10 ">
-            <h2 className="text-5xl py-2 text-teal-600 font-medium">Tony T</h2>
-            <h3 className="text-2xl py-1">Developer and Creator</h3>
-            <p className=" text-md py-5 leading-7 text-gray-600">
+            <h2 className="text-5xl py-2 text-teal-600 font-medium md:text-6xl">Tony T</h2>
+            <h3 className="text-2xl py-1 md:text-3xl">Developer and Creator</h3>
+            <p className=" text-md py-5 leading-7 text-gray-600 md:text-xl  max-w-xl mx-auto">
               Passionate creative with expertise in graphic design transitioning into the realm of
               Software Development. As a recent graduate, I bring a robust background in creative endeavors,
               driven by a fervent interest in front-end development. Eager to channel my creative prowess
@@ -28,17 +39,100 @@ export default function Home() {
               to deliver impactful solutions.
             </p>
           </div>
-          <div className="text-5xl flex justify-center gap-16 py-3 text-gray-600">
-          <AiFillLinkedin />
-          <AiFillYoutube />
-          <AiFillTwitterCircle />
+          <div className="text-5xl flex justify-center gap-16 py-3 text-gray-600">  {/* hover?? */}
+            <AiFillLinkedin />
+            <AiFillYoutube />
+            <AiFillTwitterCircle />
           </div>
-          <div className="relative mx-auto bg-gradient-to-b from-teal-500 rounded-full w-80 h-80 mt-20 overflow-hidden">
-            <Image src={sukuna} layout='fill' objectFit='cover'  />
-
+          <div className="relative mx-auto py-2 bg-gradient-to-b from-teal-500 rounded-full w-80 h-80 mt-20 overflow-hidden border-teal-500 border-8 md:h-96 md:w-96">
+            <Image src={sukuna} layout='fill' objectFit='cover' />
           </div>
         </section>
 
+        {/* Card Section */}
+        <section>
+          <div className='lg:flex gap-10' >
+            <h3 className="text-3xl py-1 ">Services I offer</h3>
+            <p className="text-md py-2 leading-8 text-gray-600">
+              Since the beginning of my journey as a freelance designer and
+              developer, I've done remote work for
+              <span className="text-teal-500"> agencies </span>
+              consulted for <span className="text-teal-500">startups </span>
+              and collaborated with talanted people to create digital products
+              for both business and consumer use.
+            </p>
+            <p className="text-md py-2 leading-8 text-gray-600">
+              I offer from a wide range of services, including brand design,
+              programming and teaching.
+            </p>
+          </div>
+          <div className='lg:flex gap-10 justify-center'>
+            <div className='text-center  shadow-xl p-10 rounded-xl my-10'>
+              <Image src={code} width={100} height={100} className='mx-auto' />
+              <h3 className='text-lg font-medium pt-8 pb-2'>Beautiful Designs</h3>
+              <p className=' py-2'>
+                Creating chaos everywhere I go. Yuji is a hoe.</p>
+              <h4 className='py-4 text-teal-600 uppercase'>Design tools I use</h4>
+              <p className='text-gray-600 py-1'>Photoshop</p>
+              <p className='text-gray-600 py-1'>Illustrator</p>
+              <p className='text-gray-600 py-1'>Figma</p>
+            </div>
+            <div className='text-center shadow-xl p-10 rounded-xl my-10'>
+              <Image src={design} width={100} height={100} className='mx-auto' />
+              <h3 className='text-lg font-medium pt-8 pb-2'>Beautiful Designs</h3>
+              <p className=' py-2'>
+                Creating chaos everywhere I go. Yuji is a hoe.</p>
+              <h4 className='py-4 text-teal-600 uppercase'>Design tools I use</h4>
+              <p className='text-gray-600 py-1'>Photoshop</p>
+              <p className='text-gray-600 py-1'>Illustrator</p>
+              <p className='text-gray-600 py-1'>Figma</p>
+            </div>
+            <div className='text-center shadow-xl p-10 rounded-xl my-10'>
+              <Image src={consulting} width={100} height={100} className='mx-auto' />
+              <h3 className='text-lg font-medium pt-8 pb-2'>Beautiful Designs</h3>
+              <p className=' py-2'>
+                Creating chaos everywhere I go. Yuji is a hoe.</p>
+              <h4 className='py-4 text-teal-600 uppercase'>Design tools I use</h4>
+              <p className='text-gray-600 py-1'>Photoshop</p>
+              <p className='text-gray-600 py-1'>Illustrator</p>
+              <p className='text-gray-600 py-1'>Figma</p>
+            </div>
+          </div>
+        </section>
+        {/* footer?? */}
+        <section>
+          <div>
+            <h3 className='text-3xl py-1'>Portfolio</h3>
+            <p className="text-md py-2 leading-8 text-gray-600">
+              Since the beginning of my journey as a freelance designer and
+              developer, I've done remote work for
+              <span className="text-teal-500"> agencies </span>
+              consulted for <span className="text-teal-500">startups </span>
+              and collaborated with talanted people to create digital products
+              for both business and consumer use.
+            </p>
+          </div> {/* Fixed flex issue */}
+          <div className='flex flex-col gap-10 py-10 lg:flex-row lg:flex-wrap'>
+            <div className='basis-1/3 flex-1 '>
+              <Image src={web1} className='rounded-lg object-cover' width={'100%'} height={'100%'} layout='responsive' />
+            </div>
+            <div className='basis-1/3 flex-1 '>
+              <Image src={web2} className='rounded-lg object-cover' width={'100%'} height={'100%'} layout='responsive' />
+            </div>
+            <div className='basis-1/3 flex-1 '>
+              <Image src={web3} className='rounded-lg object-cover' width={'100%'} height={'100%'} layout='responsive' />
+            </div>
+            <div className='basis-1/3 flex-1 '>
+              <Image src={web4} className='rounded-lg object-cover' width={'100%'} height={'100%'} layout='responsive' />
+            </div>
+            <div className='basis-1/3 flex-1 '>
+              <Image src={web5} className='rounded-lg object-cover' width={'100%'} height={'100%'} layout='responsive' />
+            </div>
+            <div className='basis-1/3 flex-1 '>
+              <Image src={web6} className='rounded-lg object-cover' width={'100%'} height={'100%'} layout='responsive' />
+            </div>
+          </div>
+        </section>
       </div>
     </main>
   );
